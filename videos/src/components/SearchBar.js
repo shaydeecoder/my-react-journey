5bd501a67be643dcef0c7705ae123207d4e79795
shaydeecoder <shaydeecoder@gmail.com>
2021-08-24 07:06:43 +0100

    added videos project along side components & JSX folders

---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  /**
+   *
+   * @param {ObjectConstructor} event
+   * @todo Make sure we call callback from parent component
+   */
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onFormSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label htmlFor="video-search">Video Search</label>
+            <input
+              type="text"
+              name="video-search"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
