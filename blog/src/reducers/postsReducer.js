989275378d09b713d4e13ce2e2dee4510511a2c4
shaydeecoder <shaydeecoder@gmail.com>
2021-09-20 08:22:30 +0100

    Loaded blog posts from jsonPlaceholder API

---
+++ blog/src/reducers/postsReducer.js
@@ -0,0 +1,16 @@
+const postsReducer = (state = [], action) => {
+  // if (action.type === "FETCH_POSTS") {
+  //   return action.payload;
+  // }
+
+  // return state;
+
+  switch (action.type) {
+    case "FETCH_POSTS":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default postsReducer;
