d1fb2d9e76f22fec09d7cffa63a2648755ec130d
shaydeecoder <shaydeecoder@gmail.com>
2021-11-04 14:26:40 +0100

    Accessed data from context object with <LanguageContext.Comsumer>

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -2,14 +2,14 @@ import { Component } from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends Component {
-  static contextType = LanguageContext;
-
   render() {
-    // console.log(this.context);
-
-    const text = this.context === "english" ? "Submit" : "Voorleggen";
-
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {(value) => (value === "english" ? "Submit" : "Voorleggen")}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
