3a8d5a473d9fa18b9a46c971928979ea1f2f4405
shaydeecoder <shaydeecoder@gmail.com>
2021-11-04 13:59:45 +0100

    Added translate logic to Field.js

--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -1,10 +1,15 @@
 import { Component } from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Field extends Component {
+  static contextType = LanguageContext;
+
   render() {
+    const text = this.context === "english" ? "Name" : "Naam";
+
     return (
       <div className="ui field">
-        <label htmlFor="name">Name</label>
+        <label htmlFor="name">{text}</label>
         <input type="text" name="name" id="name" />
       </div>
     );
